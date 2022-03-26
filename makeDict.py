0fc73ed1b0c3bc0b2f791ef81f7fe69b363b89ba
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-03-25 20:53:10 -0600

    adding new files

---
+++ makeDict.py
@@ -0,0 +1,10 @@
+import csv
+
+with open("targetsPunctuated.txt") as f:
+	lines=[line.rstrip('\n').upper() for line in f]
+
+modelText=lines[0]
+for x in range(1,len(lines)):
+	modelText=modelText+" "+lines[x]
+
+lexiconWords=modelText.split()
